249d7d8606da062f71de9fa66cbffe4a733d935b
JD Mulcahy <mr.mulcahy@icloud.com>
2025-11-29 15:21:52 -0700

    Initial Commit

---
+++ SelfServiceDashboard/SelfServiceDashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelfServiceDashboardApp.swift
+//  SelfServiceDashboard
+//
+//  Created by Ironside on 11/29/25.
+//
+
+import SwiftUI
+
+@main
+struct SelfServiceDashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
